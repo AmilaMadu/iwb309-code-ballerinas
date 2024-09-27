7995437370f7fc0751773f118d7c83b33148aee4
YasodaLAE <yasodalasiru@gmail.com>
2024-09-27 21:09:12 +0530

    Added routes to pages in App.jsx

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Appointment = () => {
   return (
     <div>
-      
+        
     </div>
   )
 }
 
-export default Home
+export default Appointment
