0ff2052212b7eed610f91335ff68e6159489e28c
YasodaLAE <yasodalasiru@gmail.com>
2024-09-27 20:54:07 +0530

    Updated App.jsx and Home.jsx. Added 'Home' name to Home.jsx and Routes to home page in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>
+    <div className='mx-4 sm:mx-[10%]'>
 
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   )
 }
