8c646335b86db94e7bfdb0bb6587fd3729b1859f
AmilaMadu <amilaarck@gmail.com>
2024-09-28 13:20:05 +0530

    01:00:00 ----- updated works

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../Components/Header'
 
 const Home = () => {
   return (
     <div>
-
+      <Header/>
     </div>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary' : "#5f6FFF"
+      }
+    },
   },
   plugins: [],
 }
