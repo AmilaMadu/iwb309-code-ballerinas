62fed5623827b7019041f800d49f7e97319c2ec6
yaseemarusiru <yaseemarusiru@gmail.com>
2024-10-09 22:21:01 +0530

    Adjusted the picture in front page

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20'>
       {/* ---------Left Side -------- */}
-      <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
+      <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-4 m-auto md:py-[6.5vw] md:mb-[-20px]'>
         <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>
           Book Appointments <br /> Easily
         </p>
