f811efbf2b9b42dd6e6e636053494943f70b7f90
yaseemarusiru <yaseemarusiru@gmail.com>
2024-10-09 22:04:11 +0530

    removed doctors

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -16,26 +16,19 @@ import upload_icon from './upload_icon.png'
 import stripe_logo from './stripe_logo.png'
 import razorpay_logo from './razorpay_logo.png'
 import doc1 from './doc1.png'
-import doc2 from './doc2.png'
 import doc3 from './doc3.png'
-import doc4 from './doc4.png'
 import doc5 from './doc5.png'
 import doc6 from './doc6.png'
 import doc7 from './doc7.png'
-import doc8 from './doc8.png'
 import doc9 from './doc9.png'
-import doc10 from './doc10.png'
 import doc11 from './doc11.png'
 import doc12 from './doc12.png'
 import doc13 from './doc13.png'
-import doc14 from './doc14.png'
 import doc15 from './doc15.png'
 import Dermatologist from './Dermatologist.svg'
 import Gastroenterologist from './Gastroenterologist.svg'
 import General_physician from './General_physician.svg'
-import Gynecologist from './Gynecologist.svg'
 import Neurologist from './Neurologist.svg'
-import Pediatricians from './Pediatricians.svg'
 
 
 export const assets = {
@@ -64,16 +57,8 @@ export const specialityData = [
         image: General_physician
     },
     {
-        speciality: 'Gynecologist',
-        image: Gynecologist
-    },
-    {
         speciality: 'Dermatologist',
         image: Dermatologist
-    },
-    {
-        speciality: 'Pediatricians',
-        image: Pediatricians
     },
     {
         speciality: 'Neurologist',
@@ -101,20 +86,6 @@ export const doctors = [
         }
     },
     {
-        _id: 'doc2',
-        name: 'Dr. Emily Larson',
-        image: doc2,
-        speciality: 'Gynecologist',
-        degree: 'MBBS',
-        experience: '3 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
-        fees: 60,
-        address: {
-            line1: '27th Cross, Richmond',
-            line2: 'Circle, Ring Road, London'
-        }
-    },
-    {
         _id: 'doc3',
         name: 'Dr. Sarah Patel',
         image: doc3,
@@ -125,20 +96,6 @@ export const doctors = [
         fees: 30,
         address: {
             line1: '37th Cross, Richmond',
-            line2: 'Circle, Ring Road, London'
-        }
-    },
-    {
-        _id: 'doc4',
-        name: 'Dr. Christopher Lee',
-        image: doc4,
-        speciality: 'Pediatricians',
-        degree: 'MBBS',
-        experience: '2 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
-        fees: 40,
-        address: {
-            line1: '47th Cross, Richmond',
             line2: 'Circle, Ring Road, London'
         }
     },
@@ -185,20 +142,6 @@ export const doctors = [
         }
     },
     {
-        _id: 'doc8',
-        name: 'Dr. Timothy White',
-        image: doc8,
-        speciality: 'Gynecologist',
-        degree: 'MBBS',
-        experience: '3 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
-        fees: 60,
-        address: {
-            line1: '27th Cross, Richmond',
-            line2: 'Circle, Ring Road, London'
-        }
-    },
-    {
         _id: 'doc9',
         name: 'Dr. Ava Mitchell',
         image: doc9,
@@ -209,20 +152,6 @@ export const doctors = [
         fees: 30,
         address: {
             line1: '37th Cross, Richmond',
-            line2: 'Circle, Ring Road, London'
-        }
-    },
-    {
-        _id: 'doc10',
-        name: 'Dr. Jeffrey King',
-        image: doc10,
-        speciality: 'Pediatricians',
-        degree: 'MBBS',
-        experience: '2 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
-        fees: 40,
-        address: {
-            line1: '47th Cross, Richmond',
             line2: 'Circle, Ring Road, London'
         }
     },
@@ -265,20 +194,6 @@ export const doctors = [
         fees: 50,
         address: {
             line1: '17th Cross, Richmond',
-            line2: 'Circle, Ring Road, London'
-        }
-    },
-    {
-        _id: 'doc14',
-        name: 'Dr. Ryan Martinez',
-        image: doc14,
-        speciality: 'Gynecologist',
-        degree: 'MBBS',
-        experience: '3 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
-        fees: 60,
-        address: {
-            line1: '27th Cross, Richmond',
             line2: 'Circle, Ring Road, London'
         }
     },
