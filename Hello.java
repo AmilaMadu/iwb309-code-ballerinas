487d385b11b287ca97b06c08029ee7ac74439d36
AmilaMadu <amilaarck@gmail.com>
2024-10-11 10:55:07 +0530

    amila

--- Hello.java
+++ Hello.java
@@ -11,10 +11,6 @@ public class Hello{
         System.out.println("Hello World");
         System.out.println("Hello World");
 
-        for(int i=0; i<10; i++){
-            System.out.println("Hello World");
-        }
-
     }
 }
 // Lahiru
